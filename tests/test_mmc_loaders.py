56b64a4239770d6ac899c2ed9c128f9ee4f0b6c4
David Marx <david.marx84@gmail.com>
2022-04-08 11:20:16 -0700

    added loader attribute tests

--- tests/test_mmc_loaders.py
+++ tests/test_mmc_loaders.py
@@ -1,7 +1,22 @@
 import pytest
 
+from mmc.modalities import TEXT, IMAGE
+
+
 def test_loader_import():
     from mmc.loaders import OpenAiClipLoader
+
+
+def test_loader_attrs():
+    from mmc.loaders import OpenAiClipLoader
+    ldr = OpenAiClipLoader(id='RN50')
+    assert ldr.architecture == 'clip'
+    assert ldr.publisher == 'openai'
+    assert ldr.id =='RN50'
+    assert len(ldr.modalities) == 2
+    assert ldr.supports_modality(TEXT)
+    assert ldr.supports_modality(IMAGE)
+
 
 def test_load_oai_clip():
     from mmc.loaders import OpenAiClipLoader
