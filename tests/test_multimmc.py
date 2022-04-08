78993a1be59163bdce9898becf6d00564e4dcaff
David Marx <david.marx84@gmail.com>
2022-04-08 14:37:03 -0700

    added test for multimmc.compare

--- tests/test_multimmc.py
+++ tests/test_multimmc.py
@@ -1,5 +1,6 @@
 from mmc.multimmc import MultiMMC
 from mmc.modalities import TEXT, IMAGE, AUDIO
+import PIL
 
 def test_init_perceptor():
     perceptor = MultiMMC(TEXT, IMAGE)
@@ -45,3 +46,18 @@ def test_supports_modality_name():
     assert perceptor._supports_mode('image')
     assert perceptor._supports_mode('text')
     assert not perceptor._supports_mode('audio')
+
+def test_compare_text2img():
+    perceptor = MultiMMC(TEXT, IMAGE)
+    perceptor.load_model(
+        architecture='clip', 
+        publisher='openai', 
+        id='RN50',
+        )
+    text_pos = "a photo of a dog"
+    text_neg = "a painting of a cat"
+    img = PIL.Image.open('./tests/assets/marley_birthday.jpg').resize((250,200))
+    v_pos = perceptor.compare(image=img, text=text_pos)
+    v_neg = perceptor.compare(image=img, text=text_pos)
+    logger.debug(v_pos, v_neg)
+    assert v_pos > v_neg
