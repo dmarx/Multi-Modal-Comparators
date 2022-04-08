b096d07e615b7f56139915b239dd66ba564b7e39
David Marx <david.marx84@gmail.com>
2022-04-07 20:19:01 -0700

    added tests

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -5,3 +5,12 @@ def test_import_module():
 
 def test_import_REGISTRY():
     from mmc.registry import REGISTRY
+
+def test_empty_search_REGISTRY():
+    from mmc.registry import REGISTRY
+    REGISTRY.find()
+
+def test_clip_search_REGISTRY():
+    from mmc.registry import REGISTRY
+    hits = REGISTRY.find(architecture='clip')
+    assert len(hits) > 1
